b1a01ee1bb56445d54ea9e192e8f7818d19be6dd
TingWu44 <Wu20160424@gmail.com>
2025-03-19 12:04:27 +0800

    0319

--- 0312-practice/reg.php
+++ 0312-practice/reg.php
@@ -7,7 +7,14 @@ if (isset($_POST["acct"])){
         $acct_existed=false;
            if(file_exists($filename)){
               $fp=fopen($filename,"r");
-              while(($member)){}
+              while(($member=fgetcsv($fp,100)!==FALSE)){
+                if(0==strcmp($member[0],$_POST["acct"])&& password_verify($_POST["pass"],$member)){
+                    printf("alert('歡迎登入，%s');",$member[1]);
+                    printf("location.herf='reg.php';");
+                    break;
+                }
+              }
+              }
            }
         $fp=fopen("member.csv","a");
         fputcsv($fp,[$_POST["acct"],$_POST["name"],
@@ -16,7 +23,7 @@ if (isset($_POST["acct"])){
     fclose($fp);
     printf("location.herf='login.php';");
     }
-}
+
 
 
 ?>
@@ -29,11 +36,11 @@ if (isset($_POST["acct"])){
     <body>
         <H1>註冊會員</H1>
         <form method="post">
-        <p>帳　　號:<input type ="text" name="acct"></p>
-        <p>顯示名稱:<input type ="text" name="name"></p>
-        <p>密　　碼:<input type ="password" name="pass1"></p>
-        <p>確認密碼:<input type ="password" name="pass2"></p>
-        <p><input type="submit"></p>
+        <p>帳　　號:<input type ="text" name="acct" placeholder="登入用的帳號"></p>
+        <p>顯示名稱:<input type ="text" name="name" placeholder="登入後的顯示名稱"></p>
+        <p>密　　碼:<input type ="password" name="pass1" placeholder="登入用的密碼"></p>
+        <p>確認密碼:<input type ="password" name="pass2" placeholder="確認兩次密碼相同"></p>
+        <p><input type="submit" value="註冊"></p>
         </form>
      </body>
 </html>
